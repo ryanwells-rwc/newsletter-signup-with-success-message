b11d4cc7ae9d319f73c1763a4153bdefda25a302
Ryan Wells <ryanwellsconsulting@gmail.com>
2026-06-21 17:28:15 -0700

    Init repo.

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form');
+const emailInput = document.getElementById('email');
+const emailError = document.getElementById('email-error');
+const initialContent = document.getElementById('initial-content');
+const successContent = document.getElementById('success-content');
+const dismissButton = document.getElementById('dismiss-button');
+const emailAddress = document.getElementById('email-address');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Reset previous errors
+  emailInput.classList.remove('error');
+  emailError.textContent = '';
+  emailError.classList.remove('error');
+
+  // Check validity using built-in checkValidity()
+  if (!emailInput.checkValidity()) {
+    // Prevent actual form submission
+    //event.preventDefault();
+
+    // Apply custom error styles and messages
+    emailInput.classList.add('error');
+    emailError.classList.add('error');
+
+    if (emailInput.validity.valueMissing || emailInput.validity.typeMismatch) {
+      emailError.textContent = 'Valid email required';
+      return;
+    }
+  }
+
+  emailAddress.textContent = emailInput.value;
+  initialContent.classList.add('hidden');
+  successContent.classList.remove('hidden');
+});
+
+dismissButton.addEventListener('click', function () {
+  initialContent.classList.remove('hidden');
+  successContent.classList.add('hidden');
+  emailInput.value = '';
+})
